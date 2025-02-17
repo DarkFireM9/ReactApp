4b573ade909d36910521bf37882089e6deda2c5d
Aaron Hernandez <darkfire2369@outlook.com>
2025-02-17 14:35:29 -0600

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Footer from './components/Footer';
 import Button from './components/Button';
 import List from './components/List';
 import Add from './components/Add';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import ResponsiveAppBar from './components/AppBar';
+import CredentialsSignInPage from './components/Login';
+
 
 
 function App() {
@@ -25,14 +31,22 @@ function App() {
   const del = (id) => {setItems(items.filter((item) => item.id !== id))};
   return (
     <div>
-    <Header/>
-    {count}
-    <Button name={"suma"} click={sum} />
-    <Button name={"resta"} click={resta} />
-    <Button name={"mensaje"} click={() => alert("Hola Mundo")} />
-    <Add add={add}/>
-    <List items={items} onDelete={del} />
-    <Footer />
+      <BrowserRouter>
+      <ResponsiveAppBar/>
+        <Header/>
+        <Routes>
+        <Route path='/' element={<CredentialsSignInPage/>} />
+        <Route path="/add" element={<Add add={add}/>} />
+        <Route path="/items" element={<List items={items} onDelete={del} />} />
+        {/* <Route path="/user" element={} /> */}
+        {/* <Route path="/game" element={} /> */}
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    {/* {count} */}
+    {/* <Button name={"suma"} click={sum} /> */}
+    {/* <Button name={"resta"} click={resta} /> */}
+    {/* <Button name={"mensaje"} click={() => alert("Hola Mundo")} /> */}
     </div>
   );
 }
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Add = ({add}) => {
+    const navigate = useNavigate();
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
     const onSubmit = (e) => {e.preventDefault(); if (!name || !price) {alert("Ingresa algo"); return;} ; add({name: name, price: price});};
+    navigate("/add");
   return (
     <form onSubmit={onSubmit}>
       <input onChange={(e) => setName(e.target.value)} value={name} type="text" name="" id='' />
